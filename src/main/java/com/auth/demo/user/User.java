de252c37672963ca8de8588edbadeb9ea606a602
Jose Soto <josesoto1993@example.com>
2023-08-29 14:53:34 -0300

    codeclean all files (automatic IntelliJ clean)

--- src/main/java/com/auth/demo/user/User.java
+++ src/main/java/com/auth/demo/user/User.java
@@ -1,7 +1,9 @@
 package com.auth.demo.user;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
--- src/main/java/com/auth/demo/user/UserRepository.java
+++ src/main/java/com/auth/demo/user/UserRepository.java
@@ -1,9 +1,11 @@
 package com.auth.demo.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByToken(String token);
+
     Optional<User> findByUsernameAndPassword(String username, String password);
 }
