3c7c9798553a4edd3d9c1d3426e4158fa3823550
Jose Soto <josesoto1993@example.com>
2023-08-29 12:27:22 -0300

    add a error handler that WONT handle the error (just to show the example)

--- src/main/java/com/auth/demo/user/UserController.java
+++ src/main/java/com/auth/demo/user/UserController.java
@@ -1,31 +1,56 @@
 package com.auth.demo.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import static org.apache.logging.log4j.util.Strings.EMPTY;
+
 
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
 
     @PostMapping("authentication/login")
     public String login(@RequestBody UserLoginReq req) {
-        return userService.authentication(req);
+        try {
+            return userService.authentication(req);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return EMPTY;
+        }
     }
 
     @GetMapping("/public-area/ping")
     public String pingPublicArea() {
-        return "esta area es publica";
+        try {
+            return "esta area es publica";
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return EMPTY;
+        }
     }
 
     @GetMapping("/user-area/ping")
     public String pingUserArea() {
-        return "esta area es de usuarios";
+        try {
+            return "esta area es de usuarios";
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return EMPTY;
+        }
     }
 
     @GetMapping("/admin-area/ping")
     public String pingAdminArea() {
-        return "esta area es de admins";
+        try {
+            return "esta area es de admins";
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return EMPTY;
+        }
     }
 }
